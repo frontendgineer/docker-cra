ee5e4d4e90a1bbefc38b7d6ca8a9635067a533b2
Jose Garcia <frontendgineer@gmail.com>
2018-03-16 21:29:59 +0100

    logo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from 'http://localhost:8001/api/v1/proxy/namespaces/default/services/kubecra-dev:80/logo.svg';
 import './App.css';
 
 class App extends Component {
